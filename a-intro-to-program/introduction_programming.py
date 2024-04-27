09aeceae8726657cc4eb396f7704bd1b49678695
Prakash Behera <prakash@Prakashs-MacBook-Air.local>
2024-04-27 09:23:48 +0530

    intro to porgam

---
+++ a-intro-to-program/introduction_programming.py
@@ -0,0 +1,52 @@
+''''
+    This is introduction to programming 
+
+    ....
+    author : prakash
+    date   : 26/apr/2024
+    ....
+
+'''
+
+'''
+    // Introduction to programming 
+
+    A program is writing a set of instructions/codes for a specific task to be done by computer, 
+    these instructions typically written with the help of programming languages, and in programming language 
+    these instructions often called statements or codes can perform a wide range of tasks from a simple calculation 
+    to big data manipulation . And the programming languages are used to write  these instructions for a specific task to be done by the computer  . 
+    
+    Types of programming languages : 
+    1) High level language 
+    2) Medium level (Assembly language)
+    3) Low level (Machine language)
+
+    High level language
+    ---------------------
+    High level programming language is portable but requires an interpreter or compiler to convert the source code into the machine code . 
+
+    Assembly language
+    --------------------
+    Assembly language, known as medium level , is not readable by humans . Assemble languages need a translator 
+    (compiler / interpreter) to convert the assembler source code into machine code . 
+
+    It is mainly used to code the microprocessors and programming devices at the hardware level . 
+
+    Machine language
+    -------------------
+    Machine language is a low-level language made up of binary numbers or bits that a computer can understand . 
+    It is also known as machine code or object code and is extremely tough to comprehend . 
+
+    The only language that the computer understands is machine language . 
+
+    In the computer , all data like : videos , programs , pictures are represented in binary . The CPU processes this machine code or binary data as input . Then, an application or operating system gets the resulting output from the CPU and displays it visually . 
+
+    For example :  The ASCII code 01000001 represents the letter  “ A “ in machine language , yet it is shown on the screen as “ A “ . 
+
+    program : set of instructions for computers to follow, written in any programming language. 
+    programming : process of developing programs.
+    programmer : person who develops the programs.
+    programming language : computer language for written set instructions.
+
+    END..
+'''
